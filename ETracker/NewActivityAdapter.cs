789917ff358b2dacb4790644acc21b4504dc15ae
HernandezIsrael <israel.hernandez@clusmext.com>
2018-12-21 17:27:29 -0600

    Add project files.

---
+++ ETracker/NewActivityAdapter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace ETracker
+{
+    class NewActivityAdapter : BaseAdapter<string>
+    {
+
+        Activity context;
+        List<string> items;
+
+        public NewActivityAdapter(Activity context, List<string> items)
+        {
+            this.context = context;
+            this.items = items;
+        }
+
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override string this[int position]
+        {
+            get
+            {
+                return items[position];
+            }
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return items.Count;
+            }
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = convertView;
+            EditText editText = FindViewById<EditText>(Resource.Id.editTextActivityFormRow);
+            string item;
+
+            if (view == null)
+            {
+                //This means we don't get a cell or a row we can recycle, so we create one
+                view = context.LayoutInflater.Inflate(Resource.Layout.new_activity_row, null);
+            }
+
+            //Let's see where we are inside of the list
+
+            item = items[position];
+
+            //if (position == 3)
+            //{
+            //    item = "(Available to subscribers only)";
+            //    view.FindViewById<TextView>(Resource.Id.textViewActivityFormRow).SetTextColor(Color.Coral);
+            //}
+            //else
+            //{
+            //    view.FindViewById<TextView>(Resource.Id.textViewActivityFormRow).SetTextColor(Color.Black);
+            //}
+
+            switch (position)
+            {
+                case 0:
+                    
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+
+                    break;
+            }
+
+            //We have our row.
+            //Now inside of that row we have some text elements that we want to populate with data
+            view.FindViewById<TextView>(Resource.Id.textViewActivityFormRow).Text = item;
+            return view;
+        }
+
+    }
+}
---
+++ ETracker/RegisterActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace ETracker
+{
+    [Activity(Label = "Nueva Actividad")]
+    public class RegisterActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.new_activity);
+
+            List<string> labels = new List<string>();
+            ArrayAdapter arrayAdapter;
+            ListView listView = FindViewById<ListView>(Resource.Id.listViewNewActivity);
+
+            labels.Add("Responsable");
+            labels.Add("Nombre de la actividad");
+            labels.Add("Descripción de la actividad");
+            labels.Add("Cliente");
+            labels.Add("Dirección cliente");
+            labels.Add("Tel. Contacto cliente");
+            labels.Add("Fecha acordada");
+
+            arrayAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleExpandableListItem1, labels);
+
+            listView.Adapter = new NewActivityAdapter(this, labels);
+
+        }
+
+    }
+}
